74119ceece758c0458eb66b159058c684454ab29
hiroqn <hiroqn1008@gmail.com>
2015-04-02 02:32:43 +0900

    reformat code

--- gulpfile.js
+++ gulpfile.js
@@ -8,47 +8,47 @@ var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 var path = require('path');
 var AUTOPREFIXER_BROWSERS = [
-    'ie >= 10',
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23',
-    'ios >= 7',
-    'android >= 4.4',
-    'bb >= 10'
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
 ];
 
 gulp.task('styles', function () {
-    return gulp.src('app/styles/styles.less')
-        .pipe($.less({
-            paths: ['app/styles']
-        }))
-        .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
-        .pipe(gulp.dest('dist/styles'));
+  return gulp.src('app/styles/styles.less')
+    .pipe($.less({
+      paths: ['app/styles']
+    }))
+    .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    .pipe(gulp.dest('dist/styles'));
 });
 
 gulp.task('jade', function () {
-    return gulp.src('app/index.jade')
-        .pipe($.jade({
-            locals: {}
-        }))
-        .pipe(gulp.dest('dist'));
+  return gulp.src('app/index.jade')
+    .pipe($.jade({
+      locals: {}
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('serve', ['jade', 'styles'], function (cb) {
-    browserSync({
-        files: 'dist/**/*',
-        server: {
-            baseDir: ['dist']
-        },
-        open: true
-    }, cb);
-    gulp.watch(['app/**/*.jade'], ['jade',  browserSync.reload]);
-    gulp.watch(['app/**/*.less'], ['styles', browserSync.reload]);
+  browserSync({
+    files: 'dist/**/*',
+    server: {
+      baseDir: ['dist']
+    },
+    open: true
+  }, cb);
+  gulp.watch(['app/**/*.jade'], ['jade', browserSync.reload]);
+  gulp.watch(['app/**/*.less'], ['styles', browserSync.reload]);
 });
 
-gulp.task('default',  function (cb) {
+gulp.task('default', function () {
   gulp.start(['jade', 'styles']);
 });
 

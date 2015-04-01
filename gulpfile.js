33d5c3b0a5cf9a7f9603272a18cf4f925210dda6
hiroqn <hiroqn1008@gmail.com>
2015-04-01 21:02:10 +0900

    add deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
-
+var runSequence = require('run-sequence');
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var path = require('path');
 var AUTOPREFIXER_BROWSERS = [
     'ie >= 10',
     'ie_mob >= 10',
@@ -43,6 +46,45 @@ gulp.task('serve', ['jade', 'styles'], function (cb) {
     }, cb);
     gulp.watch(['app/**/*.jade'], ['jade',  browserSync.reload]);
     gulp.watch(['app/**/*.less'], ['styles', browserSync.reload]);
+});
+
+gulp.task('default',  function (cb) {
+  gulp.start(['jade', 'styles']);
+});
+
+// Deployment to GitHub using git.
+gulp.task('deploy', ['default'], function (done) {
+  runSequence('deploy:commit', 'deploy:push', done);
+});
+
+// Commit changes to production build locally.
+gulp.task('deploy:commit', function (done) {
+  var msg, im = process.argv.indexOf('-m');
+  if (im >= 0) {
+    msg = process.argv.slice(im, im + 2)[1];
+  }
+  msg = msg || 'Production build';
+  var cmd = ["git add .", "git commit -m '" + msg + "'", "git status"];
+  exec(cmd.join(' && '), {cwd: path.join(process.cwd(), 'dist')}, function (err, stdout, stderr) {
+    if (!/working directory clean/m.test(stdout)) {
+      console.log('--- ERROR log ---\n' + stderr);
+      console.log('--- STDOUT log ---\n' + stdout);
+      throw err || new Error('deploy:commit');
+    }
+    done();
+  });
+});
+
+// Push changes to remote (actual deployment).
+gulp.task('deploy:push', function (done) {
+  var cwd = path.join(process.cwd(), 'dist');
+  var args = ['push', '-u', 'origin', 'gh-pages'];
+  spawn('git', args, {cwd: cwd, stdio: 'inherit'}).on('close', function (code) {
+    if (code !== 0) {
+      throw new Error('deploy:push exit code is ' + code);
+    }
+    done();
+  });
 });
 
 gulp.task('default', ['jade', 'styles'], function (cb) {
